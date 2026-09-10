7ce0b4f0e50c0bc78f0773eac1e919becfba5e31
ibnkhaldunkheli <primemarium3@gmail.com>
2026-09-10 14:25:33 +0500

    Update PdfReader.tsx

--- components/PdfReader.tsx
+++ components/PdfReader.tsx
@@ -151,6 +151,8 @@ export default function PdfReader({
         const fitScale = (container.clientWidth / unscaledWidth) * zoom;
         const viewport = page.getViewport({ scale: fitScale });
 
+        if (!canvas) return;
+        
         const context = canvas.getContext('2d');
         if (!context) return;
 
